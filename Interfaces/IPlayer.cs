d9f2fe7bdd94ec9c2284c72a2df283dded4484dc
Stebusha <stebusha@inbox.ru>
2024-10-01 17:55:01 +0300

    upd

--- Interfaces/IPlayer.cs
+++ Interfaces/IPlayer.cs
@@ -3,16 +3,12 @@ namespace TheFool;
 public interface IPlayer{
     public string Name{get;set;}
     public int TurnNumber{get;set;}
-    // public bool SuccesfulDefended{get; set;}
-
     public bool Taken{get; set;}
     public void RefillHand(Deck deck);
-
     public List<Card> GetCards();
     public List<Card> GetCardsForAttack(Table gameTable);
 
     public void Attack(Table gameTable);
     public void Defend(Card attackingCard,Table gameTable);
-
     public void TakeAllCards(Table gameTable);
 }
