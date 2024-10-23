61f5bfd0960f1449f2fbe6a816a97aa22ce014c0
Stebusha <stebusha@inbox.ru>
2024-10-23 11:44:49 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        GameController game = new GameController();  
+        GameController game = new GameController();
         bool IsGameRepeated = false;
 
         Console.WriteLine("Введите количество реальных игроков");
@@ -14,39 +14,50 @@ class Program
         Console.WriteLine("Введите количество ботов");
         string? aiplayers = Console.ReadLine();
 
-        if(int.TryParse(players, out var playerCount)&&int.TryParse(aiplayers, out var AIplayerCount)){
+        if (int.TryParse(players, out var playerCount) && int.TryParse(aiplayers, out var AIplayerCount))
+        {
             if (playerCount + AIplayerCount > 1 && playerCount + AIplayerCount <= 2)
             {
-                game.Game(playerCount,AIplayerCount,false);
+                game.Game(playerCount, AIplayerCount, false);
             }
-            else{
+            else
+            {
                 Console.WriteLine("Сумаррное количество игроков не равно 2.");
-                IsGameRepeated=true;
+                IsGameRepeated = true;
             }
         }
-        else{
+        else
+        {
             Console.WriteLine("Некорректный ввод");
-            IsGameRepeated=true;
+            IsGameRepeated = true;
         }
-        while(!IsGameRepeated){
+
+        while (!IsGameRepeated)
+        {
             Console.ReadLine();
             Console.WriteLine("Сыграть еще раз? (да/нет)");
             string? repeat = Console.ReadLine();
-            if(repeat=="да"&&repeat!="нет"){
+
+            if (repeat == "да" && repeat != "нет")
+            {
                 Console.Clear();
-                game.Game(game.PlayerCount,game.BotPlayerCount,true);
+                game.Game(game.PlayerCount, game.BotPlayerCount, true);
                 IsGameRepeated = false;
             }
-            else{
+            else
+            {
                 string path = "C:/Users/МиненковаНА/Projects/TheFool/Scores/fools.txt";
-                if(File.Exists(path)){
+
+                if (File.Exists(path))
+                {
                     File.Delete(path);
                 }
+                
                 Console.WriteLine("\nВыход");
                 IsGameRepeated = true;
                 Console.ReadLine();
             }
         }
     }
-        
+
 }
